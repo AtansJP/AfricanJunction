dfd936ca738da433896326b63d8f0d19e256cc92
AtansJP <atansjp2@gmail.com>
2025-02-14 00:04:47 +0000

    Restored GroceryapiApplication.java and GroceryapiApplicationTests.java

---
+++ src/test/java/com/africanjunction/groceryapi/GroceryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.africanjunction.groceryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
